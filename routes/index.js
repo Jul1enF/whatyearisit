dae2ebf3e60c393918be78401d43e97d014b900d
Jul1enF <j.furic@gmail.com>
2024-06-28 11:49:09 +0200

    first setup

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/year', (req, res)=> {
+  const fullDate = new Date();
+  const date = fullDate.getFullYear();
+  res.json({year : date})
+})
+
+module.exports = router;
